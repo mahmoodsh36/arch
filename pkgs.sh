c376b2dddfd53e262d3247c281993f6e2d794517
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-09-19 13:01:09 +0300

    source env.sh and run grub-mkconfig

--- pkgs.sh
+++ pkgs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+source ./env.sh
+
 # packages written in a file, ignore lines starting with #
 list_packages_from_file() {
     grep -v '^#' "$1"
@@ -25,6 +27,9 @@ setup_yay
 # yay apparently doesnt auto resolve conflicts even with --noconfirm, so we use 'yes'
 # list_packages_from_file ~/work/arch/pkgs2.txt | yay --answerdiff None --answerclean None --mflags "--noconfirm " --needed -S -
 yes | yay --answerdiff None --answerclean None --mflags "--noconfirm " --needed -S $(list_packages_from_file ~/work/arch/pkgs2.txt)
+
+# is it weird that pacman doesnt handle this on its own when a new kernel is installed
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 cd ~/work/sxiv/
 sudo make install clean
